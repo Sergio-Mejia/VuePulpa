a76c1213b9720d5faa63b624ebb06c968f1ba61f
Sergio Mejia <sdmejiac@correo.udistrital.edu.co>
2022-10-28 14:36:27 -0500

    Commit inicial proyecto vue pulpas

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,18 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Pulpa msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import Pulpa from '@/components/HelloWorld.vue'
 
 export default {
   name: 'HomeView',
   components: {
-    HelloWorld
+    Pulpa
   }
 }
 </script>
