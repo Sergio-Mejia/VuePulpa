0b29cea668f92c9407dd373e95ffa9e4abfbd686
Sergio Mejia <sdmejiac@correo.udistrital.edu.co>
2022-10-28 20:38:40 -0500

    Implementación del home

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,69 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <div class="App" id="App">
+    <nav class="navbar navbar-expand-lg navbar-custom">
+      <div class="container-fluid">
+        <a class="navbar-brand" href="#">PULPAS</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText"
+          aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarText">
+          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+            <li class="nav-item">
+              <a class="nav-link" aria-current="page" v-on:click="loadHome" href="#">Home</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Frutas</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Pedidos</a>
+            </li>
+          </ul>
+          <button type="submit" class="btn btn-secondary">Log In</button>
+        </div>
+      </div>
+    </nav>
+
+
+    <div class="">
+      <router-view />
+    </div>
+    
+    
+    
+    <footer>
+      <p>&copy; Copyright </p>
+    </footer>
+  </div>
 </template>
+
+
+<script>
+
+export default {
+  name: 'App',
+  
+  data: function () {
+    return {
+
+    }
+  },
+  components: {
+
+  },
+  methods: {
+    loadHome: function () {
+      this.$router.push({ name: 'home' });
+    }
+  },
+  created: function () {
+    document.title = "Pulpas";
+    this.$router.push({ name: 'home' })
+  }
+}
+
+</script>
+
 
 <style>
 #app {
@@ -26,5 +85,15 @@ nav a {
 
 nav a.router-link-exact-active {
   color: #42b983;
+}
+
+.navbar-custom {
+  background-color: #389c6b;
+}
+
+.navbar-custom .navbar-brand,
+.navbar-custom .navbar-text,
+.navbar-custom .nav-link {
+  color: white;
 }
 </style>
--- src/router.js
+++ src/router.js
@@ -1,25 +1,24 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
-import HomeView from './views/HomeView.vue'
+import App from './App.vue'
+
+import home from './components/Home.vue'
 
 const routes = [
-  {
-    path: '/',
-    name: 'home',
-    component: HomeView
-  },
-  {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ './views/AboutView.vue')
-  }
+    {
+        path: '/',
+        name: 'root',
+        component: App
+    },
+    {
+        path: '/home',  
+        name: 'home',
+        component: home
+    },
 ]
 
 const router = createRouter({
-  history: createWebHashHistory(),
-  routes
+    history: createWebHashHistory(),
+    routes
 })
 
 export default router
